632a703560824f067e27f19bccda1833386df4a3
Yujie Wang <79264663+Christol-Jalen@users.noreply.github.com>
2024-10-21 21:22:11 +0800

    up

--- leetcode/046.cpp
+++ leetcode/046.cpp
@@ -2,7 +2,37 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution1 { // 标记法
+private:
+    vector<int> path;
+    vector<vector<int>> result;
+    void backTrack(vector<int> nums, vector<int> used){
+        if(path.size() == nums.size()){
+            result.push_back(path);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(used[i] == true) continue; // 此数已用
+            used[i] = true;
+            path.push_back(nums[i]);
+            backTrack(nums, used);
+            used[i] = false;
+            path.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> used(nums.size(), 0); // 标记
+        backTrack(nums, used);
+        return result;
+    }
+};
+
+// =====================================================
+
+class Solution2 { // 交换法
 private:
     vector<int> path;
     vector<vector<int>> result;
