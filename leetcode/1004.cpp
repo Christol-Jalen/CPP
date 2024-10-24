f6fdec8fe1fa850c2c474fe69eb68e9d7a4ecbe7
Yujie Wang <79264663+Christol-Jalen@users.noreply.github.com>
2024-10-24 22:14:34 +0800

    up

--- leetcode/1004.cpp
+++ leetcode/1004.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-// ================方法一：滑动窗口================
+
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
@@ -18,42 +18,3 @@ public:
         return ans;
     }
 };
-
-
-// ================方法二：回溯法================
-#include <vector>
-
-void backtracking(vector<int> &a, int k, int &result, int flipNum) {
-  if (flipNum == k) {
-    // 回溯
-    int length = 0;
-    int n = a.size();
-    for (int i = 0; i < n; ++ i) {
-      if (a[i] == 1) {
-        length += 1;
-      } else if (a[i] == 0) {
-        result = max(result, length);
-        length = 0;
-      }
-    }
-    return;
-  }
-  int n = a.size();
-  for (int i = 0; i < n; ++ i) {
-    if (a[i] == 0) {
-      a[i] = 1;
-      flipNum += 1;
-      backtracking(a, k, result, flipNum);
-      flipNum -= 1;
-      a[i] = 0;
-    }
-  }
-} 
-
-int main() {
-  vector<int> a = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
-  int k = 3;
-  int result = 0;
-  backtracking(a, k, result, 0);
-  cout << result << endl;
-}
